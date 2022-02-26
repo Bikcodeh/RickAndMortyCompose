67d8a215010a5732a70a9450255b88585f335b2d
Bikcode <bikcodeh@gmail.com>
2022-02-25 21:53:21 -0500

    add drawables, strings, dimensions, colors and constants

---
+++ app/src/main/java/com/bikcode/rickandmortycompose/ui/theme/Dimensions.kt
@@ -0,0 +1,8 @@
+package com.bikcode.rickandmortycompose.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+val CHARACTER_ITEM_HEIGHT = 300.dp
+val LARGE_PADDING = 20.dp
+val MEDIUM_PADDING = 15.dp
+val SMALL_PADDING = 10.dp
